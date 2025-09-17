e615489d5421f0e027207a5a6a2e808f0b6e2990
Daella123 <daellsine@gmail.com>
2025-09-17 12:56:15 +0200

    called deposit method that accepts cash

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 ATM atm=new ATM();
+atm.deposit(12000);
     }
 }
