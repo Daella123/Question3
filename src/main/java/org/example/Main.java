839a1e1d58f119938f437bdfd354f840ebd92f77
Daella123 <daellsine@gmail.com>
2025-09-17 12:59:35 +0200

    called deposit method that uses mobile transfer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         ATM atm=new ATM();
         atm.deposit(12000);
-        atm.deposit(12000);
+        atm.deposit("check009",700);
+        atm.deposit("0789456789",400.98);
+
     }
 }
